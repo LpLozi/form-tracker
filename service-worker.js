d324b54a07cca6b678845643250f06d783d8fd31
LpLozi <av.iskhuseyin@gmail.com>
2026-08-13 14:38:47 +0300

    Refresh FORM cache v1.5.6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='form-v1.5.5';
+const CACHE='form-v1.5.6';
 const ASSETS=['/','/index.html','/app-base.html','/manifest.webmanifest','/upgrade.css','/upgrade.js','/nutrition-plus.css','/nutrition-plus.js','/workout-plus.css','/workout-plus.js','/assets/form-logo.svg','/api/icon'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
